920264d41e107259599592e3918166ceefb71038
Matthias Bärtschi <matt@matt-b.ch>
2018-11-08 10:33:15 +0100

    added code

---
+++ NeoHub/NetSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeoHub
+{
+    public class NetSettings
+    {
+        public string Net { get; set; }
+        public int[] MainNetPorts { get; set; }
+        public int[] TestNetPorts { get; set; }
+
+        public int[] GetPorts()
+        {
+            if (this.Net == NetConstants.MAIN_NET)
+            {
+                return MainNetPorts;
+            }
+
+            return TestNetPorts;
+        }
+    }
+
+    public class NetConstants
+    {
+        public const string MAIN_NET = "MainNet";
+        public const string TEST_NET = "TestNet";
+        public const string PRIVATE_NET = "PrivateNet";
+    }
+}
---
+++ NeoHub/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Neo;
+using Neo.Persistence.LevelDB;
+
+namespace NeoHub
+{
+    public class Program
+    {
+        public static NeoSystem NeoSystem;
+
+        public static void Main(string[] args)
+        {
+            InitializeNeoSystem(args);
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+
+        private static void InitializeNeoSystem(string[] args)
+        {
+            LevelDBStore store = new LevelDBStore(NeoSettings.Default.DataDirectoryPath);
+            NeoSystem = new NeoSystem(store);
+            NeoSystem.StartNode(NeoSettings.Default.NodePort, NeoSettings.Default.WsPort);
+
+            
+
+            CreateWebHostBuilder(args).Build().Run();
+        }
+    }
+}
